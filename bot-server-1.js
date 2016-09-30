a2a2c6c2a3d7ab2988c611a6cd44d9ea353c0387
Karan Bhargava <karanb@valyoo.in>
2016-10-01 05:20:42 +0530

    files changeshttpclient changes

--- bot-server-1.js
+++ bot-server-1.js
@@ -3,7 +3,6 @@
 let Wit = null;
 let interactive = null;
 var Client = require('node-rest-client').Client;
-var res = "";
 var botData = "";
 var httpClient = new Client();
 
@@ -71,126 +70,131 @@ const actions = {
             return resolve(context);
         });
     },
-    cancelOrder({context, entities}) {
-        //console.log("------ Cancellation Function Called ! ---------");
-        //console.log("Order No : " ,JSON.stringify(entities));
-        try {
-            //console.log(context);
-            //console.log(entities);
-            console.log("Processing your order for cancellation...");
-            var orderId = entities["order_id"][0]["value"];
-            //var paymentMethod = entities["method"][0]["value"];
-            var request =  {
-                "initiated_by": "Bot Batola",
-                "payment_method": "source",
-                "reason_detail": "",
-                "reason_id": 156,
-                "source": "Bot Batola"
-            };
-            res = "";
-            res = clientRequest("http://test-athena.lenskart.com:8041/"+orderId+"/cancel-invoice","POST",request);
-            setTimeout(function(){
-                if(res!="" && res.statusCode >=200 && res.statusCode<=300 ) {
-                    console.log(res.message);
-                    botData = res.message+"<br/>";
-                } else {
-                    console.log("Order cancellation failed... Please try after some time !!");
-                    botData = "Order cancellation failed... Please try after some time !!<br/>";
-                }
-            },1000);
-        } catch(e) {
-            console.log("Order cancellation failed... Please try after some time !!");
-            botData = "Order cancellation failed... Please try after some time !!<br/>";
-        }
-        return Promise.resolve();
-    },
 
-    getOrderStatus({context, entities}) {
-        //console.log("------ Order Status Function Called ! ---------");
-        //console.log("Order No : " ,JSON.stringify(entities));
-        //console.log("Processing your order Information...");
-        try {
-            var orderId = entities["order_id"][0]["value"];
-            var request =  {
-                "normalSearchData": {
-                    "searchOrderParam": orderId
-                }
+  cancelOrder({context, entities}) {
+    //console.log("------ Cancellation Function Called ! ---------");
+    //console.log("Order No : " ,JSON.stringify(entities));
+    try {
+          //console.log(context);
+          //console.log(entities);
+          console.log("Processing your order for cancellation...");
+          var orderId = entities["order_id"][0]["value"];
+          //var paymentMethod = entities["method"][0]["value"];
+          var request =  {
+            "initiated_by": "Bot Batola",
+            "payment_method": "source",
+            "reason_detail": "",
+            "reason_id": 156,
+            "source": "Bot Batola"
+          };
+          var args = {
+            data: request,
+            headers: { "Content-Type": "application/json" }
+          };
+          httpClient.post("http://test-athena.lenskart.com:8041/"+orderId+"/cancel-invoice", args, function (data, response) {
+            data["statusCode"] = response.statusCode;
+            if(data!="" && data.statusCode >=200 && data.statusCode<=300 ) {
+              console.log(data.message);
+              botData = data.message+"<br/>";
+            } else {
+              console.log("Order cancellation failed... Please try after some time !!");
+              botData = "Order cancellation failed... Please try after some time !!<br/>";
             }
-            res = "";
-            res = clientRequest("http://test-athena.lenskart.com:8765/api/v2/order/search/order?start=0&rows=100","POST",request);
-            setTimeout(function(){
-                if(res!="" && res.statusCode >=200 && res.statusCode<=300 && res.numFound > 0 ) {
-                    //console.log("Order Information : ");
-                    //console.log("Order State : " + res["orderList"][0]["state"][0]);
-                    console.log("Your order current status  is " + res["orderList"][0]["status"][0]);
-                    botData = "Your order current status  is " + res["orderList"][0]["status"][0]+"<br/>";
-                } else {
-                    console.log("No data found for order number " + orderId + ", Please verify your order number again !!");
-                    botData = "No data found for order number " + orderId + ", Please verify your order number again !!<br/>";
-                }
-            },1000);
-        } catch(e) {
-            console.log("Some error occured ... Please try after some time !!");
-            botData = "Some error occured ... Please try after some time !!<br/>";
-        }
-        return Promise.resolve();
-    },
+          });
+    } catch(e) {
+      console.log("Order cancellation failed... Please try after some time !!");
+      botData = "Order cancellation failed... Please try after some time !!<br/>";
+    }
+    return Promise.resolve();
+  },
 
-    validatePhone({context, entities}) {
-        var regex = /^\d{10}$/;
-        if (regex.test(entities["phone_number"][0]["value"])) {
-            console.log("phone number is valid");
-            botData = "phone number is valid<br/>";
-        } else {
-            console.log("Please enter a valid phone number !!");
-            botData = "Please enter a valid phone number !!<br/>";
-        }
-    },
+  getOrderStatus({context, entities}) {
+    //console.log("------ Order Status Function Called ! ---------");
+    //console.log("Order No : " ,JSON.stringify(entities));
+    //console.log("Processing your order Information...");
+    try {
+        var orderId = entities["order_id"][0]["value"];
+        var request =  {
+                          "normalSearchData": {
+                                                "searchOrderParam": orderId
+                                              }
+                        }
+        var args = {
+          data: request,
+          headers: { "Content-Type": "application/json" }
+        };
+        httpClient.post("http://test-athena.lenskart.com:8765/api/v2/order/search/order?start=0&rows=100", args, function (data, response) {
+            data["statusCode"] = response.statusCode;
+            if(data!="" && data.statusCode >=200 && data.statusCode<=300 && data.numFound > 0 ) {
+              console.log("Your order current status  is " + data["orderList"][0]["status"][0]);
+              botData = "Your order current status  is " + data["orderList"][0]["status"][0]+"<br/>";
+            } else {
+              console.log("No data found for order number " + orderId + ", Please verify your order number again !!");
+              botData = "No data found for order number " + orderId + ", Please verify your order number again !!<br/>";
+            }
+        });
+    } catch(e) {
+      console.log("Some error occured ... Please try after some time !!");
+      botData = "Some error occured ... Please try after some time !!<br/>";
+    }
+    return Promise.resolve();
+  },
+
+  validatePhone({context, entities}) {
+      var regex = /^\d{10}$/;
+      if (regex.test(entities["phone_number"][0]["value"])) {
+        console.log("phone number is valid");
+        botData = "phone number is valid<br/>";
+      } else {
+        console.log("Please enter a valid phone number !!");
+        botData = "Please enter a valid phone number !!<br/>";
+      }
+  },
 
     getEligibleOrders({context, entities}) {
-        //console.log("------ Order Status Function Called ! ---------");
-        //console.log("Order No : " ,JSON.stringify(entities));
-        //console.log("Processing your order Information...");
-        try {
-            var phone = entities["phone_number"][0]["value"];
-            var request =  {
-                "advancedSearchData": {
-                    "telephone": phone
-                }
+    try {
+        var phone = entities["phone_number"][0]["value"];
+        var request =  {
+                          "advancedSearchData": {
+                                                "telephone": phone
+                                              }
+                        }
+        var args = {
+        data: request,
+          headers: { "Content-Type": "application/json" }
+        };
+        httpClient.post("http://test-athena.lenskart.com:8765/api/v2/order/search/order?start=0&rows=100", args, function (data, response) {
+            data["statusCode"] = response.statusCode;
+            //console.log(data);
+            //console.log(response);
+            if(data!="" && data.statusCode >=200 && data.statusCode<=300 && data.numFound > 0 ) {
+              console.log("Latest orders are " + phone + " :- ");
+              botData = "Latest orders are " + phone + " :- <br/>";
+              var i =0;
+              var str= [];
+              for (i = 0; i < 5; i++) {
+                str.push(data["orderList"][i]["increment_id"]);
+              }
+              console.log(str.join(", ") + "....");
+              botData = botData + str.join(", ") + "....<br/>";
+            } else {
+              console.log("No data found for phone no " + phone + ", Please verify your phone number again !!");
+              botData = "No data found for phone no " + phone + ", Please verify your phone number again !!<br/>";
             }
-            res = "";
-            res = clientRequest("http://test-athena.lenskart.com:8765/api/v2/order/search/order?start=0&rows=100","POST",request);
-            setTimeout(function(){
-                if(res!="" && res.statusCode >=200 && res.statusCode<=300 && res.numFound > 0 ) {
-                    //console.log("Order Information : ");
-                    //console.log("Order State : " + res["orderList"][0]["state"][0]);
-                    console.log("Latest orders are " + phone + " :- ");
-                    botData = "Latest orders are " + phone + " :- <br/>";
-                    var i =0;
-                    var str= [];
-                    for (i = 0; i < 5; i++) {
-                        str.push(res["orderList"][i]["increment_id"]);
-                    }
-                    console.log(str.join(", ") + "....");
-                    botData = botData + str.join(", ") + "....<br/>";
-                } else {
-                    console.log("No data found for phone no " + phone + ", Please verify your phone number again !!");
-                    botData = "No data found for phone no " + phone + ", Please verify your phone number again !!<br/>";
-                }
-            },1000);
-        } catch(e) {
-            console.log("Some error occured ... Please try after some time !!");
-            botData = "Some error occured ... Please try after some time !!<br/>";
-        }
-        return Promise.resolve();
-    },
-    deliveryEstimate({context, entities}) {
-        console.log("------ Delivery Estimate Function Called ! ---------");
+        });
+    } catch(e) {
+      console.log("Some error occured ... Please try after some time !!");
+      botData = "Some error occured ... Please try after some time !!<br/>";
+    }
+    return Promise.resolve();
+  },
+  deliveryEstimate({context, entities}) {
+        //console.log("------ Delivery Estimate Function Called ! ---------");
         //console.log("Order No : " ,JSON.stringify(entities));
         try {
             var orderId = entities["order_id"][0]["value"];
             httpClient.get("http://athena.lenskart.com:9090/shipping/estimate/" + orderId, function (data, response) {
-                console.log(response);
+                //console.log(response);
                 data["statusCode"] = response.statusCode;
 
                 if(data !="" && data.statusCode >=200 && data.statusCode<=300 ) {
@@ -224,7 +228,6 @@ function clientRequest(url, method, request) {
             data["statusCode"] = response.statusCode;
             //console.log(data);
             //console.log(response);
-            res = data;
             return data;
         });
     } else if( method == "POST" ) {
@@ -238,7 +241,6 @@ function clientRequest(url, method, request) {
             data["statusCode"] = response.statusCode;
             //console.log(data);
             //console.log(response);
-            res = data;
             return data;
         });
     }
